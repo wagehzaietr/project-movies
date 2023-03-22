00d611186deaed55489a3ba1bdc1f02c59ee1987
wajih zaietr <107758708+wajihzaietr@users.noreply.github.com>
2023-03-23 02:05:12 +0400

    test2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    ghPages({
-      // Options
-      repo: 'https://github.com/wagehzaietr/project-movies',
-      base: "/project-movies/"
-    })
-  ]
-});
+  plugins: [react()],
+})
